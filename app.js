3e54f1be03f4361f86d691ba004bf11e06dd8c81
dan <daniloplay2009@hotmail.com>
2018-05-16 13:22:01 -0300

    naoseimaisoquecomentar

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/src/app/vis-hoje/vis-hoje.component.html'))
+  res.sendFile(path.join(__dirname, 'dist/index.html'))
 });
 
 const port = process.env.PORT || '3001';
